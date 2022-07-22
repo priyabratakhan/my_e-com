5e6c363d36b98d8a775bba8cc11c2ae1c5e528b9
Priyabrata Khan <93465026+priyabratakhan@users.noreply.github.com>
2022-07-22 10:49:35 +0530

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className='footer_div'>
+        <div>
+          <span>Customer Service</span>
+          <span>Order Status</span>
+          <span>Guest Returns</span>
+          <span>Shipping & Return Policy</span>
+          <span>Gift Cards</span>
+          <span>FAQ</span>
+          <span>Contact Us</span>
+        </div>
+        <div>
+          <span>About Us</span>
+          <span>About Our Brand</span>
+          <span>The Nordy Club</span>
+          <span>Store Locator</span>
+          <span>All Brands</span>
+          <span>Careers</span>
+          <span>Get Email Updates</span>
+          <span>Our Blog</span>
+          <span>Nordy Podcast</span>
+        </div>
+        <div>
+          <span>Our Rack & The Community</span>
+          <span>Corporate Social Responsibility</span>
+          <span>Diversity, Inclusion & Belonging</span>
+          <span>Big Brothers Big Sisters</span>
+          <span>Donate Clothes</span>
+        </div>
+        <div>
+          <span>Our Card</span>
+          <span>Apply for a Our card</span>
+          <span>Pay My Bill</span>
+          <span>Manage my Our Card</span>
+        </div>
+        <div>
+          <span>Our, Inc.</span>
+          <span>India</span>
+          <span>Trunk Club</span>
+          <span>HauteLook</span>
+          <span>Investor Relations</span>
+          <span>Press Releases</span>
+          <span>Our Media Network</span>
+        </div>
+        <div>
+          <div>Dowload Our Apps</div>
+          <div>
+            <i class='uil uil-facebook-f'></i>
+            <i class='uil uil-twitter'></i>
+            <i class='uil uil-instagram'></i>
+            <i class='uil uil-linkedin-alt'></i>
+          </div>
+        </div>
+      </div>
+      <span>
+        <span>Your Privacy Rights</span>
+        <span>Do Not Sell My Info</span>
+        <span>Terms & Conditions</span>
+        <span>Internet Based Ads</span>
+        <span>India Supply Chain Act</span>
+        <span>2022 Our Rack</span>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
